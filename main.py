54666306220a02498d5ff05a3e5fa329a1bd79c6
v_v_moon <vladimir.mun.47@gmail.com>
2025-04-27 20:39:18 +0500

    протаскиваем нужные данные

--- main.py
+++ main.py
@@ -12,11 +12,11 @@ bot = telebot.TeleBot(API_TOKEN)
 print("Бот запущен")
 
 # Создание базы данных
-connection = sqlite3.connect('remind.db')
+connection = sqlite3.connect('remind.db', check_same_thread=False)
 sql = connection.cursor()
 sql.execute('CREATE TABLE IF NOT EXISTS reminders (id INTEGER PRIMARY KEY, user_id INTEGER, reminder TEXT, time TEXT, status TEXT)')
 connection.commit()
-connection.close()
+# connection.close()
 print("База данных создана")
 
 # Стартовое сообщение
@@ -31,18 +31,18 @@ def start_message(message):
 @bot.message_handler(commands=['remind'])
 
 def remind_message(message):
-    reminder_text = message.text
     bot.send_message(message.from_user.id, "Напиши текст напоминания")
-    bot.register_next_step_handler(message, get_remind_text, reminder_text=reminder_text)
+    bot.register_next_step_handler(message, get_remind_text)
 
 def get_remind_text(message):
+    reminder_text = message.text
     bot.send_message(message.from_user.id, "Напиши время в формате ЧЧ:ММ")
-    bot.register_next_step_handler(message, get_remind_time)
+    bot.register_next_step_handler(message, get_remind_time, reminder_text_from_prev_step=reminder_text)
 
-def get_remind_time(message):
-    sql.execute('INSERT INTO reminders (user_id, reminder, time, status) VALUES (?, ?, ?, ?)', (message.from_user.id, message.text, message.text, 'active'))
+def get_remind_time(message, reminder_text_from_prev_step):
+    sql.execute('INSERT INTO reminders (user_id, reminder, time, status) VALUES (?, ?, ?, ?)', (message.from_user.id, reminder_text_from_prev_step, message.text, 'active'))
     connection.commit()
-    connection.close()
+    # connection.close()
     bot.send_message(message.from_user.id, "Напоминание создано")
     print('Все в порядке, продолжаем:)')
 
