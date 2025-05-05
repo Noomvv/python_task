34967748ad1a1798ee7a9f730c633b6502057aa5
v_v_moon <vladimir.mun.47@gmail.com>
2025-05-06 02:14:17 +0500

    not fix

--- main2.py
+++ main2.py
@@ -13,17 +13,18 @@ def send_welcome(message):
     bot.send_message(message.from_user.id, f"Привет, {message.from_user.first_name}! 👽", reply_markup=buttons.remind_button())
 
 @bot.message_handler(content_types=['text'])
-def ask_reminder(message):
-    bot.send_message(message.from_user.id, "Введите через сколько секунд и текст напоминания (пример: 10 Купить хлеб)")
+def ask_text(message):
+    remind_text = bot.send_message(message.from_user.id, "Что тебе напомнить?")
+    bot.register_next_step_handler(remind_text, ask_delay)
 
-@bot.message_handler(content_types=['text'])
-def set_reminder(message):
-    parts = message.text.split(' ', 1)
-    delay = int(parts[0])
-    reminder_text = parts[1].strip()
-    threading.Timer(delay, lambda: bot.send_message(message.chat.id, reminder_text)).start()
-    bot.send_message(message.chat.id, "Напоминание установлено!")
+def ask_delay(message):
+    reminder_text = message.text
+    delay_text = bot.send_message(message.from_user.id, "Через сколько секунд напомнить?")
+    bot.register_next_step_handler(delay_text, reminder_text, set_reminder)
 
-if __name__ == '__main__':
-    print('Бот запущен')
-    bot.infinity_polling()
+def set_reminder(message, reminder_text):
+    delay = int(message.text)
+    threading.Timer(delay, bot.send_message(message.from_user.id, reminder_text).start()
+                    bot.send_message(message.from_user.id, "Готово!"))
+
+bot.polling(none_stop=True)
