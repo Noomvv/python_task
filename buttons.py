a171ffa098511fcb5b30da66ba39c4df1dbe6f68
v_v_moon <vladimir.mun.47@gmail.com>
2025-05-06 14:03:56 +0500

    дополнения

--- buttons.py
+++ buttons.py
@@ -3,6 +3,6 @@ from telebot import types
 
 def remind_button():
     kb = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    remind_btn = types.KeyboardButton('Напомнить')
+    remind_btn = types.KeyboardButton('Напомнить👄ç')
     kb.add(remind_btn)
     return kb
--- main2.py
+++ main2.py
@@ -6,27 +6,30 @@ from buttons import remind_button
 
 load_dotenv()
 API_TOKEN = os.getenv('API_TOKEN')
+DEV_ID = os.getenv('DEV_ID')
 bot = telebot.TeleBot(API_TOKEN)
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
     bot.send_message(message.from_user.id, f"Привет, {message.from_user.first_name}! 👽", reply_markup=buttons.remind_button())
 
+
 @bot.message_handler(content_types=['text'])
 def ask_text(message):
-    remind_text = bot.send_message(message.from_user.id, "Что тебе напомнить?")
+    remind_text = bot.send_message(message.from_user.id, "Что тебе напомнить?🫦")
     bot.register_next_step_handler(remind_text, ask_delay)
 
 def ask_delay(message):
     reminder_text = message.text
-    delay_text = bot.send_message(message.from_user.id, "Через сколько секунд напомнить?")
+    delay_text = bot.send_message(message.from_user.id, "Через сколько секунд напомнить?🥸")
     bot.register_next_step_handler(delay_text, set_reminder, reminder_text)
 
 def set_reminder(message, reminder_text):
     delay = int(message.text)
-    t = threading.Timer(delay, lambda: bot.send_message(message.from_user.id, reminder_text))
+    t = threading.Timer(delay, lambda: bot.send_message(message.from_user.id, f'{message.from_user.first_name}, не забудь {reminder_text}'))
     t.start()
-    bot.send_message(message.from_user.id, "Готово!")
+    bot.send_message(message.from_user.id, "Готово!🙂‍↕️")
+    bot.send_message(DEV_ID, f'Вашим ботом воспользовался юзер {message.from_user.first_name}')
     
-print("Бот запущен")
+print("Я готов!!!")
 bot.polling(none_stop=True)
